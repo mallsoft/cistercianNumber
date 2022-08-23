465cee0dbcbc8b364e41ceb206e5046465c45fda
hans <hkmelaa@hotmail.com>
2022-08-23 19:39:52 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import CistercianNumber from '$lib/CistercianNumber.svelte';
+
+	let numbers = [1, 2, 3, 4, 5, 6, 100, 200, 300, 400, 500, 3500, 5000, 6000, 7000];
+</script>
+
+<h1>Preview</h1>
+<ul>
+	{#each numbers as n}
+		<li>
+			<CistercianNumber value={n} />
+			{n}
+		</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		list-style: none;
+		display: flex;
+		height: 6em;
+	}
+
+	li {
+		margin: 5px;
+		border: 1px solid black;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
